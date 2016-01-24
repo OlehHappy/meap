814a52825adbf8e0f229803e359da8d6b755158e
Oleh Daybov <olegtranslator@gmail.com>
2016-01-24 20:09:10 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@
             $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
             $('body').delay(350).css({'overflow':'visible'});
         });
-        
-        
+
+
         // This is the jQuery for Page Smooth Scrolling Feature
 	    $(function() {
 	        $('a.page-scroll').bind('click', function(event) {
 	            var $anchor = $(this);
 	            $('html, body').stop().animate({
 	            scrollTop: $($anchor.attr('href')).offset().top
-	            }, 
+	            },
 	                1500, 'easeInOutExpo');
 	                event.preventDefault();
 	            });
@@ -26,11 +26,11 @@
 
     	//Fullscreen Background Image Slideshow
         $.backstretch([
-            "img/bg/01.jpg", 
+            "img/bg/01.jpg",
             "img/bg/02.jpg",
             "img//bg/03.jpg"
         ], {duration: 3000, fade: 750});
-        
+
 
         // Contact form toggle hide/show
         $("#hide").click(function(){
@@ -50,7 +50,7 @@
             $this.data('counted', false);
             $this.html('0');
         });
-        
+
         $(window).bind('scroll', function() {
             var speed   = 3000;
             $('.count').each(function() {
@@ -71,10 +71,10 @@
                 }
             });
         }).triggerHandler('scroll');
-        
+
         // Reviews Carousel using Owl Carousel Plugin
             $("#owl-reviews").owlCarousel({
-                navigation : false, 
+                navigation : false,
                 slideSpeed : 300,
                 paginationSpeed : 400,
                 singleItem:true,
@@ -98,9 +98,25 @@
                 itemsTabletSmall: [550,1],
                 itemsMobile : [ 480,1],
             });
-            
-            
-        // Content Animation Effects using Wow.js Plugin 
+
+        // Video Carousel using Owl Carousel Plugin
+            $('#owl-video').owlCarousel({
+                items : 6,
+                navigation : true,
+                navigationText: [
+          "<i class='fa fa-chevron-left fa-2x'></i>",
+          "<i class='fa fa-chevron-right fa-2x'></i>"
+          ],
+                pagination: false,
+                itemsDesktop : [1199,3],
+                itemsDesktopSmall : [980,2],
+                itemsTablet: [768,1],
+                itemsTabletSmall: [550,1],
+                itemsMobile : [ 480,1]
+            });
+
+
+        // Content Animation Effects using Wow.js Plugin
         var wow = new WOW( {
             boxClass:     'wow',      // animated element css class (default is wow)
             animateClass: 'animated', // animation css class (default is animated)
@@ -109,21 +125,21 @@
             live:         true        // act on asynchronously loaded content (default is true)
             });
         wow.init();
-        
+
 
         //Mailchimp Subscription Integration
         $('.mailchimp').ajaxChimp({
             callback: mailchimpCallback,
-            url: "http://jennylynpereira.us8.list-manage.com/subscribe/post?u=d594f4d2197a205cf487f3525&amp;id=a9e603bfd5" //Replace this with your own mailchimp post URL. Don t remove the "". Just paste the url inside "".  
+            url: "http://jennylynpereira.us8.list-manage.com/subscribe/post?u=d594f4d2197a205cf487f3525&amp;id=a9e603bfd5" //Replace this with your own mailchimp post URL. Don t remove the "". Just paste the url inside "".
         });
 
         function mailchimpCallback(resp) {
              if (resp.result === 'success') {
                 $('.subscription-success').html('<i class="fa fa-thumbs-up"></i><br/>' + resp.msg).fadeIn(1000);
                 $('.subscription-error').fadeOut(500);
-                
+
             } else if(resp.result === 'error') {
                 $('.subscription-error').html('<i class="fa fa-warning"></i><br/>' + resp.msg).fadeIn(1000);
-            }  
+            }
         };
     });
